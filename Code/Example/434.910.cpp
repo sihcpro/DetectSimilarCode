dd8138edf6c00da625f78cd80c4a89473be65e85
sihc <tvquocchi@gmail.com>
2018-04-20 15:13:08 +0700

    fix bug transfer

---
+++ Code/Example/434.910.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define mod 1e9+7
+#define X first
+#define Y second
+#define pb push_back
+#define mp make_pair
+#define FOR(i,a,b) for (int i = (a); i <= (b); i++)
+#define FORE(i,a,b) for (int i = (a); i >= (b); i--)
+
+typedef long long ll;
+typedef pair<ll,ll> ii;
+priority_queue <ll,vector<int>,greater<int> > heap_min;
+ll a[1000005];
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    int l,n;
+    cin >> l >> n;
+    FOR(i,1,n)
+        cin >> a[i];
+    sort(a + 1, a + n + 1);
+    if (n%2 == 1)
+        cout << min(a[n/2+1] - 0, l - a[n/2+1]) << " ";
+    else
+        cout << min(a[n/2] - 0, l - a[n/2+1]) << " ";
+    cout << max(a[1] - 0, max(l - a[1], max(a[n] - 0, l - a[n])));
+    return 0;
+}
---
+++ Code/Example/464.995.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+int main(){
+	int l, n;
+	scanf("%d %d", &l, &n);
+	int A[n];
+	for(int i=0; i<n; i++){
+		scanf("%d", &A[i]);
+	}
+	int min = A[n/2];
+	l-min<min-1 ? min = l-min : min = min-1;
+	int max;
+	l-A[0]>A[n-1]-1 ? max = l-A[0] : max = A[n-1]-1;
+	printf("%d %d", min, max);
+	return 0;
+}
